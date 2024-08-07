35dcc11270958e75af752b7eb05e89dd6b553d05
kekW <panicua12@gmail.com>
2024-08-08 02:04:42 +0300

    chore,doc: XPATHs are constants now, basic func doc/comments

--- selenium_scraper/scraper.py
+++ selenium_scraper/scraper.py
@@ -5,13 +5,20 @@ from selenium_scraper.dto import CourseDTO, CourseType
 from selenium_scraper.utils import WebDriverManager
 from selenium_scraper.utils import write_to_json
 
+from selenium_scraper.constants import (
+    BASE_URL,
+    OUTPUT_FILENAME,
+    COURSES_ELEMENTS_XPATH,
+    COURSE_DESCRIPTION_XPATH,
+    COURSE_FLEX_XPATH,
+    COURSE_FULL_TIME_XPATH,
+    COURSE_NAME_XPATH,
+)
+
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-
-BASE_URL = "https://mate.academy/"
-OUTPUT_FILE = "result.json"
 
 
 def courses_scraper(url: str):
@@ -19,27 +26,28 @@ def courses_scraper(url: str):
     with WebDriverManager(headless=True) as manager:
         driver = manager.driver
         driver.get(url)
+
+        # Get all courses
         courses_elements = driver.find_elements(
-            By.XPATH, "//*[@id='all-courses']/div/div/div/div"
+            By.XPATH, COURSES_ELEMENTS_XPATH
         )[:-1]
+
         courses = create_courses(courses_elements)
-        write_to_json(filename=OUTPUT_FILE, courses=courses)
+        write_to_json(filename=OUTPUT_FILENAME, courses=courses)
     logging.info("Scraping completed")
 
 
 def create_courses(courses: list[WebElement]) -> list[CourseDTO]:
+    """Creates list of CourseDTO objects from list of web elements"""
     list_of_courses = []
     for i, course in enumerate(courses):
         try:
-            name = course.find_element(By.XPATH, ".//a/h3").text
-            description = course.find_element(By.XPATH, ".//p[2]").text
-            flex = course.find_elements(
-                By.XPATH, ".//*[@data-qa='fx-course-details-button']"
-            )
-            full_time = course.find_elements(
-                By.XPATH,
-                ".//*[@data-qa='fulltime-course-more-details-button']",
-            )
+            name = course.find_element(By.XPATH, COURSE_NAME_XPATH).text
+            description = course.find_element(
+                By.XPATH, COURSE_DESCRIPTION_XPATH
+            ).text
+            flex = course.find_elements(By.XPATH, COURSE_FLEX_XPATH)
+            full_time = course.find_elements(By.XPATH, COURSE_FULL_TIME_XPATH)
 
             if flex:
                 list_of_courses.append(
