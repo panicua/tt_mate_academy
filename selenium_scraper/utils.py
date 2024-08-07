2f4043e415d6d18ce916a7e3f72ed500bb05e785
kekW <panicua12@gmail.com>
2024-08-07 21:17:30 +0300

    chore: moved write and class_to_dict funcs to utils.py

--- selenium_scraper/utils.py
+++ selenium_scraper/utils.py
@@ -1,5 +1,11 @@
+import json
+import logging
+from typing import List
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+
+from selenium_scraper.dto import CourseDTO
 
 
 class WebDriverManager:
@@ -20,3 +26,20 @@ class WebDriverManager:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.driver:
             self.driver.quit()
+
+
+def courses_to_dict(courses: List[CourseDTO]) -> List[dict]:
+    return [
+        {
+            "course_name": course.name,
+            "course_description": course.description,
+            "course_type": course.type.value,
+        }
+        for course in courses
+    ]
+
+
+def write_to_json(filename: str, courses: list[CourseDTO]):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(courses_to_dict(courses), file, indent=4, ensure_ascii=False)
+    logging.info(f"Data written to {filename}")
