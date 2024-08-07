58d72095ff60f4ec23e04ced55cccf61a565c9a0
kekW <panicua12@gmail.com>
2024-08-08 02:02:27 +0300

    chore: moved all the constants > constants.py

---
+++ selenium_scraper/constants.py
@@ -0,0 +1,12 @@
+# Main constants
+BASE_URL = "https://mate.academy/"
+OUTPUT_FILENAME = "result.json"
+
+
+# XPATH constants
+COURSES_ELEMENTS_XPATH = "//*[@id='all-courses']/div/div/div/div"
+
+COURSE_NAME_XPATH = ".//a/h3"
+COURSE_DESCRIPTION_XPATH = ".//p[2]"
+COURSE_FLEX_XPATH = ".//*[@data-qa='fx-course-details-button']"
+COURSE_FULL_TIME_XPATH = ".//*[@data-qa='fulltime-course-more-details-button']"
