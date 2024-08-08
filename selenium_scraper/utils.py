fc69a0374c9fb10d5dd93506fb5e3cf80e775f54
kekW <panicua12@gmail.com>
2024-08-08 04:24:46 +0300

    chore: name of write_to_json func

--- selenium_scraper/utils.py
+++ selenium_scraper/utils.py
@@ -19,7 +19,7 @@ def courses_to_dict(courses: List[CourseDTO]) -> List[dict]:
     ]
 
 
-def write_to_json(filename: str, courses: list[CourseDTO]):
+def write_courses_to_json(filename: str, courses: list[CourseDTO]):
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(courses_to_dict(courses), file, indent=4, ensure_ascii=False)
     logging.info(f"Data written to {filename}")
